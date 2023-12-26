0c586a9ee84f22fb7c86073757a09b530b603b91
Rohitxpro <140487047+Rohitxpro@users.noreply.github.com>
2023-12-26 18:13:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Rohitxpro/MISTY",
+    "https://github.com/Rohitxpro/Misty",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
